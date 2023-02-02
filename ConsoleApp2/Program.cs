a87cf738c60be460fa24073534206121a16884af
Ronoa <Ronoa@ASUSLAPTOP>
2023-02-02 23:42:54 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 1, b = 1, c, i;
+Console.Write("Fibonacci basamak sayısını giriniz: ");
+i=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(a);
+Console.WriteLine(b);
+for(int j=2; j<i; j++)
+{
+    c = a + b;
+    a = b;
+    b = c;
+    Console.WriteLine(c);
+}
+
+
